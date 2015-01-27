35f34cb33782e7b2fc1e45f12e853991557d8d09
Matthijs Brouns <matthijs.brouns@gmail.com>
2015-01-27 21:15:50 +0100

    added zipf regresion plots

--- analysis/ema.R
+++ analysis/ema.R
@@ -33,15 +33,40 @@ calculateZipfFit <- function(row, plot=FALSE) {
     }
     rmse <- sqrt(mean((city.sizes-expected.city.sizes)^2))
     
-    if (city.sizes[1] < 500){rmse <- 3000}
+    if (city.sizes[1] < 1000){rmse <- 3000}
     rmse
 }
 
 
+calculateRSquared <- function(row, plot=FALSE) {
+    city.sizes <- sort(c(row$City.10, row$City.9, row$City.8, row$City.7, row$City.6, row$City.5, row$City.4, row$City.3, row$City.2, row$City.1), decreasing=T)
+    # A = R / P^-a
+    expected.city.sizes <- vector()
+    for (i in 1:10) { 
+        expected.city.sizes <- c(expected.city.sizes, round(1/ (i / (city.sizes[1]))))
+        
+    }
+    
+    rsquared <- 1 - round((sum(((city.sizes - expected.city.sizes)^2))/sum(((city.sizes - mean(city.sizes))^2))), 2)
+    
+    if (city.sizes[1] < 1000){rsquared <- 0}
+    
+    
+    if (plot){
+        plot(log(city.sizes), log(1:10), main="Zipf Regression",
+             xlab="ln(City Size)", ylab="ln(Rank)")
+        mtext(paste("            r^2:", rsquared, sep=" "), 1, adj=0, line=-2)
+        lines(log(expected.city.sizes), (log(1:10)),col="green")
+        
+    }
+ 
+    rsquared
+}
 
 
 for(row in 1:nrow(input)) { 
     input$sse[row] <- calculateZipfFit(input[row, ])
+    input$rsquared[row] <- calculateRSquared(input[row, ])
 }
 
 
@@ -69,11 +94,11 @@ fit <- rpart(bin ~ .,
              data = input[ , c(3:45, 61)],
              method = "class"
 )
+fit2 <- prp(fit,snip=TRUE)$obj
+prp(fit2, type=0, varlen=0, tweak=0.8, box.col=c("palegreen3", "yellow", "orange", "darkorange","darkorange", "firebrick1","firebrick1", "red")[fit$frame$yval])
 
-prp(fit, type=0, varlen=0, tweak=0.8, box.col=c("palegreen3", "yellow", "orange", "darkorange","darkorange", "firebrick1","firebrick1", "red")[fit$frame$yval])
-
-pfit<- prune(fit, cp=   fit$cptable[which.min(fit$cptable[,"xerror"]),"CP"])
-pfit <- prp(pfit,snip=TRUE)$obj
+#pfit<- prune(fit, cp=0.015)
+#pfit <- prp(pfit,snip=TRUE)$obj
 # plot the pruned tree 
 #fancyRpartPlot(fit)
 
